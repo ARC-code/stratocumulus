bbfa29bd90ef7c87b3bb13a0c824d9441dfe4295
Akseli Palen <akseli.palen@gmail.com>
2022-07-11 21:40:44 +0300

    move normalize_size under lib

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const graphologyLayout = require('graphology-layout')
 const graphologyForce = require('graphology-layout-force')
 const graphologyNoverlap = require('graphology-layout-noverlap')
 const config = require('./lib/config')
+const normalize_size = require('./lib/normalize_size')
 
 const default_color = config.default_color
 const kind_color_map = config.kind_color_map
@@ -232,13 +233,4 @@ function take_network_snapshot(path) {
         }
         `
     }
-}
-
-function normalize_size(s) {
-    if (s < sizing.min_value) { sizing.min_value = s; }
-    if (s > sizing.max_value) { sizing.max_value = s; }
-
-    let mx = (s - sizing.min_value) / (sizing.max_value - sizing.min_value);
-    let preshiftNorm = mx * (sizing.max_node_size - sizing.min_node_size);
-    return parseInt(preshiftNorm + sizing.min_node_size);
 }
