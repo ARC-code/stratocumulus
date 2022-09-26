7f1e61fc4071e7588288cb767479774623462fd1
Bryan Tarpley <bptarpley@tamu.edu>
2022-09-26 16:58:00 -0500

    implementing backend code changes to cache strata graphs using redis

--- server/adapters/arc/__init__.py
+++ server/adapters/arc/__init__.py
@@ -1,15 +1,31 @@
 import requests
 import aiohttp
 import asyncio
+import json
+import redis
 from time import sleep
 
 
-def build_stratum(channel, context={}, wait=0):
+# instantiate a redis client for caching built stratum graphs
+redis_cache = redis.Redis(host="redis", decode_responses=True)
+
+
+# this function builds strata graphs.
+#
+# "channel" represents a unique browser session
+# "request_identifier" is the request
+#   path (starting with /build_stratum) used to fire off the asynchronous task running this code, and is used as the
+#   redis cache key.
+# "context" is a dictionary containing GET variables passed to /build_stratum, containing information like the path and
+#   any facet/search specifications
+def build_stratum(channel, request_identifier, context={}, wait=0):
+
     # optional wait in seconds, as a way, for example,
     # to allow a browser time to subscribe
     sleep(wait)
 
-    make_subgraph(channel, {
+    # build skeleton for the stratum graph
+    stratum_graph = {
         'path': context['path'],
         'nodes': [
             {'id': '/arc', 'label': 'ARC', 'fixed': True, 'value': 5000, 'parent': 'self'},
@@ -22,10 +38,16 @@ def build_stratum(channel, context={}, wait=0):
             {'from': '/arc', 'to': '/arc/genres'},
             {'from': '/arc', 'to': '/arc/disciplines'}
         ]
-    })
+    }
 
+    # publish the skeleton to the browser client immediately
+    make_subgraph(channel, stratum_graph)
+
+    # fire off the "build_facets" function asynchronously. it returns an array (one item per facet),
+    # and each of those items are themselves arrays of subgraphs. we'll use these to constitute the full facet graph
+    # for the purpose of caching
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(build_facets(
+    facet_graphs = loop.run_until_complete(build_facets(
         channel,
         context['path'],
         [
@@ -36,10 +58,25 @@ def build_stratum(channel, context={}, wait=0):
         '/arc'
     ))
 
+    # now that we have an array of arrays of subgraphs, iterate over them and add each node and edge to the larger
+    # stratum graph for caching
+    for facet_graph in facet_graphs:
+        for facet_subgraph in facet_graph:
+            if 'nodes' in facet_subgraph:
+                for node in facet_subgraph['nodes']:
+                    stratum_graph['nodes'].append(node)
+
+            if 'edges' in facet_subgraph:
+                for edge in facet_subgraph['edges']:
+                    stratum_graph['edges'].append(edge)
+
+    # cache the full stratum graph so we don't have to rebuild it every time!
+    redis_cache.set(request_identifier, json.dumps(stratum_graph))
 
 
 async def build_facets(channel, path, facets, connected_to, query_params=None):
     async with aiohttp.ClientSession() as session:
+        queries = []
         for facet in facets:
             facet_query = "https://corpora.dh.tamu.edu/api/corpus/5f623b8eff276600a4f44553/ArcArtifact/?page-size=0&a_terms_{facet}={facet}.id,{facet}.label.raw".format(
                 facet=facet
@@ -49,26 +86,43 @@ async def build_facets(channel, path, facets, connected_to, query_params=None):
                 for param, val in query_params.items():
                     facet_query += "&{param}={val}".format(param=param, val=val)
 
-            print(facet_query)
+            queries.append(
+                perform_facet_query(
+                    session,
+                    channel,
+                    path,
+                    facet,
+                    connected_to,
+                    facet_query
+                )
+            )
 
-            async with session.get(facet_query) as resp:
-                data = await resp.json()
+        return await asyncio.gather(*queries, return_exceptions=True)
 
-                if 'meta' in data and \
-                        'aggregations' in data['meta'] and \
-                        facet in data['meta']['aggregations']:
 
-                    for agg_key, agg_count in data['meta']['aggregations'][facet].items():
-                        node_parts = agg_key.split('|||')
-                        make_nedge(
-                            channel,
-                            path,
-                            "{connected_to}/{facet}/{id}".format(connected_to=connected_to, facet=facet, id=node_parts[0]),
-                            node_parts[1],
-                            "{connected_to}/{facet}".format(connected_to=connected_to, facet=facet),
-                            facet,
-                            agg_count
-                        )
+async def perform_facet_query(session, channel, path, facet, connected_to, facet_query):
+    async with session.get(facet_query) as resp:
+        data = await resp.json()
+        subgraphs = []
+
+        if 'meta' in data and \
+                'aggregations' in data['meta'] and \
+                facet in data['meta']['aggregations']:
+
+            for agg_key, agg_count in data['meta']['aggregations'][facet].items():
+                node_parts = agg_key.split('|||')
+                subgraphs.append(make_nedge(
+                    channel,
+                    path,
+                    "{connected_to}/{facet}/{id}".format(connected_to=connected_to, facet=facet, id=node_parts[0]),
+                    node_parts[1],
+                    "{connected_to}/{facet}".format(connected_to=connected_to, facet=facet),
+                    facet,
+                    agg_count
+                ))
+
+        return subgraphs
+
 
 
 def make_node(channel, path, uri, label, kind=None, value=None):
@@ -89,6 +143,7 @@ def make_node(channel, path, uri, label, kind=None, value=None):
         subgraph['nodes'][0]['value'] = value
 
     make_subgraph(channel, subgraph)
+    return subgraph
 
 
 def make_nedge(channel, path, uri, label, from_uri, kind=None, value=None):
@@ -116,6 +171,7 @@ def make_nedge(channel, path, uri, label, from_uri, kind=None, value=None):
         subgraph['nodes'][0]['value'] = value
 
     make_subgraph(channel, subgraph)
+    return subgraph
 
 
 def make_subgraph(channel, graph):
--- server/stratocumulus.py
+++ server/stratocumulus.py
@@ -3,9 +3,13 @@ import uuid
 import os
 from flask import Flask, request, render_template, session
 from flask_sse import sse
+from time import sleep
 from adapters import arc
 from celery import Celery
 
+
+# instantiate and configure our Flask app, including SSE (server sent events).
+# for an example Flask+SSE app, see here: https://www.velotio.com/engineering-blog/how-to-implement-server-sent-events-using-python-flask-and-react
 app = Flask(__name__,
             static_url_path='/static',
             static_folder=os.environ['STRATO_STATIC_FOLDER'])
@@ -13,35 +17,58 @@ app.secret_key = os.environ['STRATO_SECRET_KEY']
 app.config["REDIS_URL"] = "redis://redis"
 app.register_blueprint(sse, url_prefix='/stream')
 
+# instantiate and configure our Celery client
 client = Celery(app.name, broker=app.config['REDIS_URL'])
 client.conf.update(app.config)
 
-# flask-sse example: https://www.velotio.com/engineering-blog/how-to-implement-server-sent-events-using-python-flask-and-react
 
-
+# this is the root endpoint for our app. it renders the HTML for the client
 @app.route('/')
 def index():
+    # either retrieve our unique session key or create a new one if doesn't exist
     sess_key = _get_strato_key()
+
+    # render the HTML template injecting our session key
     return render_template("index.html", key=sess_key)
 
 
+# this endpoint is called by the client when it's ready to build a "stratum," which here signifies a bird's eye view of
+# a large dataset in the form of a network graph, where "facets" are represented as nodes, and the values for each facet
+# represented as child nodes
 @app.route('/build_stratum')
 def build_stratum():
+    # retrieve our unique session key
     sess_key = _get_strato_key()
+
+    # make sure we have a path for our stratum; if not specified via GET param, assume root (/)
     path = request.args.get('path', '/')
-    context = {'path': path}
 
-    for filter in request.args.keys():
-        if filter != 'path':
-            context[filter] = request.args[filter]
-
+    # if requesting the root path, set "wait" to number of seconds to pause before sending bits of our stratum graph.
+    # the idea is to give the browser a chance to subscribe to the SSE (server sent events) feed
     wait = 0
     if path == '/':
         wait = 2
-    launch_stratum_build_job.apply_async(args=[sess_key, context, wait])
+
+    # check if we've already built this stratum before and cached the entire stratum's graph
+    cached_response = sse.redis.get(request.full_path)
+    if cached_response:
+        app.logger.info('got cached response')
+        if wait:
+            sleep(wait)
+
+        # since we have the cached response, go ahead and publish it to the SSE feed
+        sse.publish(cached_response.decode('utf-8'), type=sess_key)
+    else:
+        # since we haven't built this stratum before, we need to build it. this might involve some longer than ideal
+        # HTTP requests, so we'll do our building asynchronously by launching a Celery task
+        launch_stratum_build_job.apply_async(args=[sess_key, request.full_path, request.args, wait])
     return "Building."
 
 
+# this endpoint is for immediately publishing "subgraphs" (or small bits of the larger stratum) to the SSE stream the
+# to which the client is subscribed, allowing for the asynchronous "build_stratum" job to stream content as it goes
+# (rather than having to wait for the entire stratum to be built). this endpoint can also be used to publish the
+# entirety of a stratum cached by Redis.
 @app.route('/publish', methods=['POST'])
 def publish_graph():
     key = request.args.get('key')
@@ -49,6 +76,8 @@ def publish_graph():
     return "Sent."
 
 
+# this helper function simply returns a unique key for our session if it exists, and otherwise generates and stores the
+# key before returning it.
 def _get_strato_key():
     if 'strato_key' not in session:
         session['strato_key'] = uuid.uuid1()
@@ -56,10 +85,19 @@ def _get_strato_key():
     return session['strato_key']
 
 
+# this is the asynchronous task we use to build a stratum's graph. the idea is for this to eventually be
+# pluggable via adapter style code. for now we only have ARC registered, so our asynchronous task here just calls the
+# "build_stratum" function belonging to the "arc" module
 @client.task
-def launch_stratum_build_job(key, context, wait):
-    arc.build_stratum(key, context, wait)
+def launch_stratum_build_job(key, request_identifier, context, wait):
+    arc.build_stratum(key, request_identifier, context, wait)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+else:
+    # these lines allow us to sync up the gunicorn and Flask log handlers so Flask logging appears in Gunicorn's stdout/
+    # stderr outputs
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
