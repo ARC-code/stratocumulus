de9197494d3600075c6c4407e12d3b5e2866d5e3
Akseli Palen <akseli.palen@gmail.com>
2023-02-14 05:12:26 +0200

    make labels clickable and refactor click handling of facetable nodes

--- client/lib/strata/stratum/view/drawGraph.js
+++ client/lib/strata/stratum/view/drawGraph.js
@@ -44,6 +44,8 @@ module.exports = function (stratum, final = false) {
       newItem.setAnchor(newItem.atCenter())
       // Disable interaction with node content.
       newItem.setContentInput(false)
+      // Make it easy to find node attributes via tapspace.
+      newItem.nodeAttributes = attrs
 
       nodeGroup.addChild(newItem, nPosition)
     }
@@ -91,61 +93,45 @@ module.exports = function (stratum, final = false) {
       }
     })
 
-    // Add click event for facetable nodes
-    const facetableNodes = document.querySelectorAll('.node[data-isFacetable]')
     const facetableClickHandler = (event) => {
-      const clickedNodeId = event.target.id
-      const facetPath = clickedNodeId.replaceAll('_', '/')
-      const facetParam = event.target.getAttribute('data-facetParam')
-      const facetValue = event.target.getAttribute('data-facetValue')
-      const new_context = Object.assign({}, stratum.context)
+      const clickedItem = event.component
+      const attrs = clickedItem.nodeAttributes
+      const facetPath = attrs.id
+      const facetParam = attrs.facetParam
+      const facetValue = attrs.facetValue
+      const newContext = Object.assign({}, stratum.context)
 
-      if (new_context.hasOwnProperty(facetParam)) {
-        let current_values = new_context[facetParam].split('__')
+      if (newContext[facetParam]) {
+        const currentValues = newContext[facetParam].split('__')
 
-        if (!current_values.includes(facetValue))
-          current_values.push(facetValue)
-
-        new_context[facetParam] = current_values.join('__')
-      } else
-        new_context[facetParam] = facetValue
-
-      const nodeItem = tapspace.components.Basis.findAffineAncestor(event.target)
-      if (nodeItem) {
-        // The click emits an event "stratumrequest" which is listened on
-        // strata-level, so that individual stratum does not need to know
-        // about or control other strata.
-        const position = nodeItem.atCenter().offset(0, 0, 120)
-        stratum.emit('stratumrequest', {
-          path: facetPath,
-          context: new_context,
-          label: 'todo',
-          bgColor: 'todo',
-          position: position
-        })
+        if (!currentValues.includes(facetValue)) {
+          currentValues.push(facetValue)
+        }
+        newContext[facetParam] = currentValues.join('__')
+      } else {
+        newContext[facetParam] = facetValue
       }
+
+      // The click emits an event "stratumrequest" which is listened on
+      // strata-level, so that individual stratum does not need to know
+      // about or control other strata.
+      const position = clickedItem.atCenter().offset(0, 0, 120)
+      stratum.emit('stratumrequest', {
+        path: facetPath,
+        context: newContext,
+        label: 'todo',
+        bgColor: 'todo',
+        position: position
+      })
     }
 
-    facetableNodes.forEach(facetableNode => {
-      facetableNode.removeEventListener('click', facetableClickHandler)
-      facetableNode.addEventListener('click', facetableClickHandler)
+    const facetableItems = nodeGroup.getChildren().filter(nodeItem => {
+      return nodeItem.nodeAttributes.isFacetable
+    })
+
+    facetableItems.forEach(item => {
+      item.tappable()
+      item.on('tap', facetableClickHandler)
     })
   }
-
-  // strata[path].network = new vis.Network(strata[path].div, strata[path].data, graph_options)
-  // strata[path].network.on("click", function(params) {
-  //     if (params.nodes.length > 0) {
-  //         let clickedUri = params.nodes[0]
-  //         let node = strata[path].data.nodes.get(clickedUri)
-  //         if (node.hasOwnProperty('kind')) {
-  //             console.log(clickedUri)
-  //             let objIdRegex = new RegExp("([^/]*)$", "gm")
-  //             let objIdMatch = objIdRegex.exec(clickedUri)
-  //             let objId = objIdMatch[1]
-  //             let newContext = Object.assign({}, strata[path].context)
-  //             newContext[f_{node.kind}.id] = objId
-  //             buildStratum(clickedUri, newContext, node.label, node.color)
-  //         }
-  //     }
-  // })
 }
