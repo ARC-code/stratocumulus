7027d6b527e8b7b12abc089fc6bd11c0698d29a0
Akseli Palén <akseli.palen@gmail.com>
2023-05-06 22:24:27 -0400

    repair Stratum:prune

--- client/lib/Stratum/prune.js
+++ client/lib/Stratum/prune.js
@@ -1,28 +1,24 @@
-module.exports = (space, graph) => {
+module.exports = function () {
   // Remove all graph node and edge elements that do not anymore exist in
   // the given graph.
-  //
-  // Parameters:
-  //   space
-  //     a tapspace Space
-  //   graph
-  //     a graphology Graph
   //
 
   const renderedNodeKeys = Object.keys(this.renderedNodes)
   const renderedEdgeKeys = Object.keys(this.renderedEdges)
 
   renderedNodeKeys.forEach(nodeKey => {
-    if (!graph.hasNode(nodeKey)) {
+    if (!this.graph.hasNode(nodeKey)) {
       const stratumNode = this.renderedNodes[nodeKey]
       stratumNode.remove()
+      delete this.renderedNodes[nodeKey]
     }
   })
 
   renderedEdgeKeys.forEach(edgeKey => {
-    if (!graph.hasEdge(edgeKey)) {
+    if (!this.graph.hasEdge(edgeKey)) {
       const edgeItem = this.renderedEdges[edgeKey]
       edgeItem.remove()
+      delete this.renderedEdges[edgeKey]
     }
   })
 }
