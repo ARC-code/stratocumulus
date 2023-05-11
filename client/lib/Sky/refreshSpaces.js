ca7de73be377457262c4c75bf12de2e24f5aa478
Akseli Palén <akseli.palen@gmail.com>
2023-05-10 23:39:15 -0400

    sketch semantic zoom for current statum nodes

--- client/lib/Sky/refreshSpaces.js
+++ client/lib/Sky/refreshSpaces.js
@@ -6,30 +6,47 @@ module.exports = function () {
   // - open and close strata
   //
 
-  const stratumPath = this.findCurrentStratum()
+  // Shortcut if space still empty.
+  if (!this.currentStratumPath) {
+    return
+  }
 
-  if (this.currentStratumPath !== stratumPath) {
+  // Find possible new current stratum
+  const currentStratum = this.findCurrentStratum()
+  // Update current stratum pointer.
+  // TODO maybe store navigation somewhere else because
+  // TODO it affects also breadcrumbs and paths.
+  if (this.currentStratumPath !== currentStratum.path) {
     const previous = this.currentStratumPath
-    this.currentStratumPath = stratumPath
-
+    this.currentStratumPath = currentStratum.path
+    // Inform future navigation store that current path is changed.
     this.emit('pathchanged', {
       previousStratumPath: previous,
-      currentStratumPath: stratumPath
+      currentStratumPath: currentStratum.path
     })
   }
 
   console.log('current', currentStratum.path)
-  //
-  // // Measure nodes on that closest stratum
-  // // to see if one should be opened.
-  // const nodeItems = currentStratum.nodePlane.getChildren()
-  // const nodeMetrics = nodeItems.map(nodeItem => {
-  //   const dilation = this.viewport.measureDilation(nodeItem)
-  //   const originOnView = nodeItem.atCenter().projectTo(this.viewport, camera)
-  //   const distancePx = originOnView.getDistanceTo(viewCenter).getRaw()
-  //   const dz = viewWidth / dilation
-  //   const distance3 = Math.sqrt(distancePx * distancePx + dz * dz)
-  // })
+
+  // On the current stratum, try to find a node that could be
+  // large and central enough to be open.
+  const stratumSpace = currentStratum.space
+  const camera = this.viewport.atCamera()
+  const centerOnStratum = this.getOrigin().projectTo(stratumSpace, camera)
+  const maxDistance = this.viewport.getBoundingBox().getDiagonal().scaleBy(0.2)
+  const maybeNodeKey = currentStratum.findNodeNear(centerOnStratum, maxDistance)
+  if (maybeNodeKey) {
+    const candidateKey = maybeNodeKey
+    const candidateNode = currentStratum.getNode(candidateKey)
+    if (candidateNode.isFacetable() && !candidateNode.isFaceted()) {
+      const metric = this.viewport.measureOne(candidateNode.component)
+      console.log('candidate label', candidateNode.label)
+      console.log('candidate areaRatio', metric.areaRatio)
+      if (metric.areaRatio > 0.05) {
+        currentStratum.openNode(candidateKey)
+      }
+    }
+  }
 
   // Reveal or close labels of each stratum
   Object.values(this.strata).forEach(stratum => {
