8444ea3386e56fc2921a0836d2c0255bc20b5e55
Akseli Palen <akseli.palen@gmail.com>
2023-06-28 04:12:09 +0300

    migrate to tapspace alpha.14

--- client/lib/Stratum/recomputeBoundingCircle.js
+++ client/lib/Stratum/recomputeBoundingCircle.js
@@ -1,9 +1,7 @@
 const tapspace = require('tapspace')
 
 module.exports = function () {
-  const sphere = this.nodePlane.getBoundingCircle()
-  // TODO tapspace should return Circle, not Sphere
-  const circle = new tapspace.geometry.Circle(sphere.basis, sphere.sphere)
-
-  this.boundingCircle = circle
+  // Computation of bounding circle can be heavy, thus this method
+  // computes it at demand.
+  this.boundingCircle = this.nodePlane.getBoundingCircle()
 }
