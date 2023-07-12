0ca42b45263e303e88b1f13a1f91bec25f06f2ff
Akseli Palen <akseli.palen@gmail.com>
2023-07-12 13:38:51 +0300

    remove dead code from Sky

--- client/lib/Sky/getOrigin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-  // Get the origin point of the sky. The viewport center.
-  // Useful for estimating the position of our gaze.
-  //
-  // Return
-  //   a tapspace.geometry.Point
-  //
-  return this.viewport.atNorm(0.5, 0.5)
-}
