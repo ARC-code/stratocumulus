9afc2f5ec87ffc1e9219840d1326fca2c8b51eda
Akseli Palen <akseli.palen@gmail.com>
2023-07-14 21:58:47 +0300

    improve ContextStore: call subscribers only if state changed

--- client/lib/io/ContextStore/dispatch.js
+++ client/lib/io/ContextStore/dispatch.js
@@ -1,4 +1,8 @@
 module.exports = function (action) {
-  this.state = this.reducer(this.state, action)
-  this.subs.forEach(handler => handler())
+  const newState = this.reducer(this.state, action)
+  if (newState !== this.state) {
+    // Change occurred
+    this.state = newState
+    this.subs.forEach(handler => handler())
+  }
 }
