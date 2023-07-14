e0637d1a5a0029c41112be2832ac182d385f8e0c
Akseli Palen <akseli.palen@gmail.com>
2023-07-14 21:59:25 +0300

    use ContextStore in client-side main

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,6 +1,5 @@
-const io = require('./io')
 const tapspace = require('tapspace')
-const Context = require('./Context')
+const io = require('./io')
 const Sky = require('./Sky')
 const TimeSlider = require('./TimeSlider')
 const Toolbar = require('./Toolbar')
@@ -14,9 +13,6 @@ exports.start = function () {
   // - app bundle is cached (ok message, old versions)
   console.log('stratocumulus-client v' + clientVersion)
   console.log('tapspace.js v' + tapspace.version)
-
-  // Global filtering context
-  let context = new Context()
 
   // Open SSE stream
   io.stream.connect()
@@ -45,7 +41,6 @@ exports.start = function () {
   sky.once('first', () => {
     // TODO estimated fit to content
     viewport.scaleBy(2, viewport.atCenter())
-
     // Make viewport interactive as the space has content.
     viewportManager.enableNavigation()
   })
@@ -53,39 +48,53 @@ exports.start = function () {
   // Navigation changes current context.
   // TODO rename to 'navigation'
   sky.on('navigation', (ev) => {
-    const currentContext = ev.context
     console.log('navigation event', ev.context.toContextObject())
-    toolbar.contextForm.setContext(currentContext)
+    io.contextStore.dispatch({
+      type: 'navigation',
+      path: ev.context.toFacetPath()
+    })
   })
 
   // Connect search bar
   toolbar.on('search', (ev) => {
     // Filter strata by search query
-    // TODO update strata based on the global filtering context object.
-    sky.filterByKeyword(ev.query)
-    // Update context
-    context = context.remove('q').append('q', ev.query)
-    // Feed to context form
-    toolbar.contextForm.setContext(context)
+    io.contextStore.dispatch({
+      type: 'filter/keyword',
+      keyword: ev.query
+    })
   })
-
   toolbar.on('clear', (ev) => {
-    // Update context
-    context = context.remove(ev.parameter)
-    // Feed to context form
-    toolbar.contextForm.setContext(context)
-    // TODO clear sky context
+    io.contextStore.dispatch({
+      type: 'filter/keyword/clear'
+    })
   })
 
   // Connect time range slider
   slider.on('change', (ev) => {
-    // Update strata based on the year range
-    // TODO update strata based on the global filtering context object.
-    sky.emphasizeDecades(ev.rangeStart, ev.rangeEnd)
-    // Update context
-    const rangeValue = ev.rangeStart + 'to' + ev.rangeEnd
-    context = context.remove('r_years').append('r_years', rangeValue)
-    // Feed to context form
+    io.contextStore.dispatch({
+      type: 'filter/years',
+      rangeStart: ev.rangeStart,
+      rangeEnd: ev.rangeEnd
+    })
+  })
+
+  // Propagate context changes to the components.
+  io.contextStore.subscribe(() => {
+    const context = io.contextStore.getState()
+
+    // Refresh context widget
     toolbar.contextForm.setContext(context)
+
+    // Filter Sky by keyword
+    if (context.hasParameter('q')) {
+      const query = context.getValue('q')
+      sky.filterByKeyword(query)
+    }
+
+    // Filter Sky by years
+    if (context.hasParameter('r_years')) {
+      const range = context.getRangeValue('r_years')
+      sky.emphasizeDecades(range.rangeStart, range.rangeEnd)
+    }
   })
 }
