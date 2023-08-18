20a81d89c9c7432e09cec17420b3020ea960de6a
Akseli Palen <akseli.palen@gmail.com>
2023-08-18 17:44:39 +0300

    implement io.corpora.fetchArtifact

--- client/lib/io/corpora.js
+++ client/lib/io/corpora.js
@@ -1,3 +1,19 @@
-exports.get = (params) => {
-  // TODO a query to corpora to fetch a document meta data
+// This string will now be passed to the client by the backend.
+const corporaApiPrefix = window.stratocumulus.corporaApiPrefix
+// Use native fetch API
+const fetch = window.fetch
+
+exports.fetchArtifact = (id, callback) => {
+  // Fetch single artifact from the Corpora API.
+  //
+  // Parameters:
+  //   id
+  //     a string, the artifact ID
+  //
+  const artifactUrl = `${corporaApiPrefix}ArcArtifact/${id}/`
+
+  fetch(artifactUrl)
+    .then(response => response.json())
+    .then(art => callback(null, art))
+    .catch(err => callback(err))
 }
