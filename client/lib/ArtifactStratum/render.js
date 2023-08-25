754b8bd36737abe86f17e5da2495d73897abdf03
Akseli Palen <akseli.palen@gmail.com>
2023-08-25 20:18:22 +0300

    sketch spiral layout for cards

--- client/lib/ArtifactStratum/render.js
+++ client/lib/ArtifactStratum/render.js
@@ -1,4 +1,7 @@
 const ArtifactNode = require('../ArtifactNode')
+const STEPS = 9
+const RADIUS = 1280 // TODO use config
+const ANGLE_STEP = 2 * Math.PI / STEPS
 
 module.exports = function () {
   // @ArtifactStratum:render()
@@ -7,9 +10,9 @@ module.exports = function () {
   // This method is idempotent, thus you can call this method multiple times
   // without unexpected side effects.
   //
-  const nodePlaneOrigin = this.nodePlane.at(0, 0)
+  const nodePlaneOrigin = this.nodePlane.at(RADIUS, RADIUS)
 
-  this.artifactIds.forEach((key, i) => {
+  this.artifactIds.forEach((key, i, ids) => {
     let artifactNode = this.renderedNodes[key]
 
     if (artifactNode) {
@@ -24,9 +27,11 @@ module.exports = function () {
     }
 
     // Derive scale relative to the stratum basis.
-    artifactNode.setScale(0.4)
+    artifactNode.setScale(1.6 / (1 + i / STEPS))
     // Update position according to order.
-    const nPoint = nodePlaneOrigin.offset(i * 200, 0) // TODO spiral
+    const distance = RADIUS / (1 + i / STEPS)
+    const direction = ANGLE_STEP * i
+    const nPoint = nodePlaneOrigin.polarOffset(distance, direction)
     artifactNode.translateTo(nPoint)
   })
 
