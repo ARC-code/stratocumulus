b42d740eaa7f2792de57a224f4aa474eaff95da7
Akseli Palen <akseli.palen@gmail.com>
2023-08-28 16:11:46 +0300

    placeholders for Stratum:triggerSubstratum

---
+++ client/lib/ArtifactStratum/triggerSubstratum.js
@@ -0,0 +1,13 @@
+module.exports = function (viewport) {
+  // @ArtifactStratum:triggerSubstratum(viewport)
+  //
+  // This method implements viewport dependent behavior
+  // that subsequently initiates the loading of substratum if
+  // zoom level and other viewport conditions so allow.
+  //
+  // Parameters:
+  //   viewport
+  //     a tapspace.components.Viewport
+  //
+  throw new Error('Not implemented')
+}
