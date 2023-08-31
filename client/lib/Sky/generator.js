4ee0863ef438ff5b7df329a76aae0279d372a88f
Akseli Palen <akseli.palen@gmail.com>
2023-08-31 17:15:15 +0300

    skip prepopulating the superstrata with single node due to layout issues

--- client/lib/Sky/generator.js
+++ client/lib/Sky/generator.js
@@ -1,7 +1,6 @@
 const CategoryStratum = require('../CategoryStratum')
 const ArtifactStratum = require('../ArtifactStratum')
 const Context = require('../Context')
-const io = require('../io')
 
 module.exports = (sky, loader) => {
   // Generator for TreeLoader. Generator defines how the loaded spaces
@@ -41,30 +40,6 @@ module.exports = (sky, loader) => {
     // Maintain index of create strata.
     // TODO unnecessary overlap with loader spaces?
     sky.strata[path] = stratum
-
-    if (!context.hasParameter('page')) {
-      // Populate with a single node. This node will be upgraded later.
-      // TODO smells hacky
-      if (ev.childId && stratum.graph.order === 0) {
-        const subStratum = sky.strata[ev.childId]
-        if (subStratum) {
-          const lastFacet = subStratum.context.getLastFacet()
-          const nodeKey = subStratum.context.toNodeKey()
-          const label = io.labelStore.read(lastFacet.parameter, lastFacet.value)
-          io.graphStore.provide(context, {
-            id: nodeKey,
-            kind: lastFacet.kind, // TODO do not use parameter-based kind, sketcy
-            label: label || '', // will be replaced in load
-            is_facetable: true,
-            facet_param: lastFacet.parameter,
-            facet_value: lastFacet.value
-          })
-        }
-      }
-    }
-
-    // Attempt to render.
-    stratum.render(false, 1)
 
     // Add stratum to space via the loader.
     const spaceAdded = loader.addSpace(path, stratum.getSpace())
