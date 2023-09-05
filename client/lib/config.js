d707d6ebc9935835b394e1d16ac87071bcb05dca
Akseli Palen <akseli.palen@gmail.com>
2023-09-05 16:49:36 +0300

    build validator for server-provided configuration

--- client/lib/config.js
+++ client/lib/config.js
@@ -16,3 +16,47 @@ exports.artifacts = {
   threshold: 100, // min facetable count
   pageSize: 9
 }
+
+// Required server-provided configuration
+const required = [
+  { key: 'sseStreamUrl', type: 'string' },
+  { key: 'sseStreamKey', type: 'string' },
+  { key: 'corporaApiPrefix', type: 'string' },
+  { key: 'facetParameters', type: 'array' },
+  { key: 'filterParameters', type: 'array' },
+  { key: 'yearRange', type: 'object' },
+  { key: 'agentRoleMapping', type: 'object' }
+]
+
+exports.validate = () => {
+  // @config.validate()
+  //
+  // Validate the server-provided configuration.
+  //
+  if (!window || !window.stratocumulus) {
+    throw new Error('Missing window.stratocumulus configuration object.')
+  }
+  const conf = window.stratocumulus
+
+  for (let i = 0; i < required.length; i += 1) {
+    const schemaItem = required[i]
+    const key = schemaItem.key
+    const type = schemaItem.type
+
+    if (!conf[key]) {
+      throw new Error('Missing configuration: window.stratocumulus.' + key)
+    }
+
+    if (type === 'array') {
+      if (!Array.isArray(conf[key])) {
+        throw new Error('Invalid configuration: window.stratocumulus.' + key)
+      }
+    } else {
+      if (typeof conf[key] !== type) {
+        throw new Error('Invalid configuration: window.stratocumulus.' + key)
+      }
+    }
+  }
+
+  // No errors, everything ok
+}
